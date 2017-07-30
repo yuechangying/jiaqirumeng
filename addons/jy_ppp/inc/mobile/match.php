e355d7e4a25e37600d15fa6133fa7cedb15f5e51
ruizhang <zhang.rui@yuejian.com>
2017-07-30 11:50:35 +0800

    调试择偶条件

--- addons/jy_ppp/inc/mobile/match.php
+++ addons/jy_ppp/inc/mobile/match.php
@@ -1,10 +1,7 @@
 <?php
-include '../../../debug/chromephp/ChromePhp.php';
 
 global $_W, $_GPC;
-
-ChromePhp::log($_W);
-ChromePhp::log(1);
+var_dump($_w);
 if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') == false) {
     $weixin = 0;
 
