75f6a7f1b962762c85c6e37e84b8dcd88b716dff
yuechangying <718679544@qq.com>
2017-08-19 11:22:37 +0800

    免费查看管理员邮件

--- addons/jy_ppp/inc/mobile/lianxi.php
+++ addons/jy_ppp/inc/mobile/lianxi.php
@@ -126,8 +126,10 @@ global $_W,$_GPC;
 			}
 			else
 			{
+                printLog(basename(__FILE__) . ",line=" . __LINE__ . ", YYY mid=" . $mid);
+
 				$sitem=pdo_fetch("SELECT * FROM ".tablename('jy_ppp_setting')." WHERE weid=".$weid);
-				$lianxi=pdo_fetchall("SELECT a.id,a.mid as mid,b.avatar,b.nicheng,b.province,b.brith,b.sex,c.height FROM ".tablename('jy_ppp_xinxi')." as a left join ".tablename('jy_ppp_member')." as b on a.mid=b.id left join ".tablename('jy_ppp_basic')." as c on a.mid=c.mid WHERE a.weid=".$weid." AND a.sendid=".$mid." AND a.type!=0 GROUP BY a.mid ORDER BY a.createtime DESC LIMIT 10 ");
+				$lianxi=pdo_fetchall("SELECT a.id,a.mid as mid,b.avatar,b.nicheng,b.province,b.brith,b.sex,b.type,c.height FROM ".tablename('jy_ppp_xinxi')." as a left join ".tablename('jy_ppp_member')." as b on a.mid=b.id left join ".tablename('jy_ppp_basic')." as c on a.mid=c.mid WHERE a.weid=".$weid." AND a.sendid=".$mid." AND a.type!=0 GROUP BY a.mid ORDER BY a.createtime DESC LIMIT 10 ");
 
 
 				include $this->template('lianxi');
